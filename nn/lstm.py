99564f08d9c4d23f9b132fadad82c1be33ceaa50
Tyler Payne <tpayne95@gmail.com>
2015-04-06 00:27:34 -0400

    latest

--- nn/lstm.py
+++ nn/lstm.py
@@ -106,7 +106,7 @@ class lstm(object):
 					tarval = encode(x[t+1])
 					self.forward(inval)
 					targets.append(tarval)
-					if utils.vocab[x[t+1]] == asarray(self.outputs[-1]).argmax(axis=1):
+					if utils.vocab[decode(x[t+1])] == asarray(self.outputs[-1]).argmax(axis=1):
 						correct += 1
 				#print(targets)
 				acc = float(correct)/float(count)
